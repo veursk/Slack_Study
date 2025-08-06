6f58820447f080121837653f7d755c53a8f67b8c
Leo <hbcho@sbs.co.kr>
2025-08-06 13:51:28 +0900

    feat: Channel 페이지 세팅

---
+++ front/pages/Channel/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Workspace from '@layouts/Workspace';
+
+const Channel = () => {
+  return (
+    <Workspace>
+      <div>로그인 하신것을 축하드립니다</div>
+    </Workspace>
+  );
+};
+
+export default Channel;
