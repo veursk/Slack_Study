1e7d3b4689f0ee48a650179778f150d5edc92f7b
Leo <hbcho@sbs.co.kr>
2025-08-07 13:44:08 +0900

    rename: layout 아래 폴더 추가

--- front/client.tsx
+++ front/client.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import App from '@layouts/App';
+import App from '@layouts/App/App';
 
 const domNode = document.getElementById('app');
 const root = createRoot(domNode!);
