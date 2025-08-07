9b4f0bbc86b512749695e48f4ad9dae04ac36c57
Leo <hbcho@sbs.co.kr>
2025-08-07 13:43:41 +0900

    feat: Menu 컴포넌트 구현
    1. 오른쪽 상단에 프로필을 눌렀을 때 뜨는 메뉴 컴포넌트

---
+++ front/components/Menu/index.tsx
@@ -0,0 +1,28 @@
+import React, { CSSProperties, PropsWithChildren, useCallback } from 'react';
+import { CloseModalButton, CreateMenu } from './styles';
+
+interface Props {
+  children: React.ReactNode;
+  show: boolean;
+  onCloseModal: () => void;
+  style: CSSProperties;
+  closeButton?: boolean;
+}
+
+const Menu: React.FC<Props> = ({ children, style, show, onCloseModal, closeButton = true }) => {
+  // 나를 눌렀을 때는 메뉴가 안 닫히고, 부모를 눌렀을 때는 닫혀야함
+  const stopPropagation = useCallback((e: any) => {
+    e.stopPropagation();
+  }, []);
+
+  return (
+    <CreateMenu onClick={onCloseModal}>
+      <div style={style} onClick={stopPropagation}>
+        {closeButton && <CloseModalButton onClick={onCloseModal}>&times;</CloseModalButton>}
+        {children}
+      </div>
+    </CreateMenu>
+  );
+};
+
+export default Menu;
