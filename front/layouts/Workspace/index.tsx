b7b9e641ac8a7ad8eb5ad477ed1d68b1cfe74968
Leo <hbcho@sbs.co.kr>
2025-08-08 09:47:48 +0900

    feat: + 버튼으로 Workspace 만드는 기능 구현
    1. <Workspace> <WorkspaceButton> 구현
    
    2. <Modal> 컴포넌트 사용
    
    3. onCreateWorkspace 함수에서 API 연동

--- front/layouts/Workspace/index.tsx
+++ front/layouts/Workspace/index.tsx
@@ -2,10 +2,12 @@ import React, { useCallback, useState } from 'react';
 import useSWR from 'swr';
 import fetcher from '@utils/fetcher';
 import axios from 'axios';
-import { Routes, Route, Navigate } from 'react-router';
+import { toast } from 'react-toastify';
+import { Routes, Route, Navigate, Link } from 'react-router';
 import loadable from '@loadable/component';
 
 import {
+  AddButton,
   Channels,
   Chats,
   Header,
@@ -14,19 +16,35 @@ import {
   ProfileImg,
   ProfileModal,
   RightMenu,
+  WorkspaceButton,
   WorkspaceName,
   Workspaces,
   WorkspaceWrapper,
 } from './styles';
 import gravatar from 'gravatar';
 import Menu from '@components/Menu';
+import { IUser } from '@typings/db';
+import { Button, Input } from '@pages/SignUp/styles';
+import useInput from '@hooks/useInput';
+import Modal from '@components/Modal';
 
 const Channel = loadable(() => import('@pages/Channel'));
 const DirectMessage = loadable(() => import('@pages/DirectMessage'));
 
 const Workspace: React.FC<React.PropsWithChildren> = ({ children }) => {
   const [showUserMenu, setShowUserMenu] = useState(false);
-  const { data, error, isLoading, mutate } = useSWR('http://localhost:3095/api/users', fetcher);
+  const [ShowCreateWorkspaceModal, setShowCreateWorkspaceModal] = useState(false);
+  const [newWorkspace, onChangeNewWorkspace, setNewWorkspace] = useInput('');
+  const [newUrl, onChangeNewUrl, setNewUrl] = useInput('');
+
+  const {
+    data: userData,
+    error,
+    isLoading,
+    mutate,
+  } = useSWR<IUser | false>('http://localhost:3095/api/users', fetcher);
+
+  console.log(userData);
 
   const onLogout = useCallback(() => {
     axios
@@ -38,11 +56,54 @@ const Workspace: React.FC<React.PropsWithChildren> = ({ children }) => {
       });
   }, []);
 
-  const onClickUserProfile = useCallback(() => {
+  const onClickUserProfile = useCallback((e: any) => {
+    e.stopPropagation();
     setShowUserMenu((prev) => !prev);
   }, []);
 
-  if (!data) {
+  const onClickCreateWorkspace = useCallback(() => {
+    setShowCreateWorkspaceModal(true);
+  }, []);
+
+  const onCreateWorkspace = useCallback(
+    (e: any) => {
+      e.preventDefault(); // form submit을 하면 default로 새로고침되니까 항상 막아야함.
+
+      if (!newWorkspace || !newWorkspace.trim()) return; // 띄어쓰기도 막아야해서 trim 조건도 추가
+      if (!newUrl || !newUrl.trim()) return;
+
+      axios
+        .post(
+          'http://localhost:3095/api/workspaces',
+          {
+            workspace: newWorkspace,
+            url: newUrl,
+          },
+          {
+            withCredentials: true,
+          },
+        )
+        .then(() => {
+          mutate();
+          setShowCreateWorkspaceModal(false);
+          setNewWorkspace('');
+          setNewUrl('');
+        })
+        .catch((err) => {
+          console.dir(err);
+          toast.error(error.response?.data, {
+            position: 'bottom-center',
+          });
+        });
+    },
+    [newWorkspace, newUrl],
+  );
+
+  const onCloseModal = useCallback(() => {
+    setShowCreateWorkspaceModal(false);
+  }, []);
+
+  if (!userData) {
     return <Navigate to="/login" />;
   }
 
@@ -51,13 +112,13 @@ const Workspace: React.FC<React.PropsWithChildren> = ({ children }) => {
       <Header>
         <RightMenu>
           <span onClick={onClickUserProfile}>
-            <ProfileImg src={gravatar.url(data.nickname, { s: '28px', d: 'retro' })} alt={data.nickname} />
+            <ProfileImg src={gravatar.url(userData.nickname, { s: '28px', d: 'retro' })} alt={userData.nickname} />
             {showUserMenu && (
               <Menu style={{ right: 0, top: 38 }} show={showUserMenu} onCloseModal={onClickUserProfile}>
                 <ProfileModal>
-                  <img src={gravatar.url(data.nickname, { s: '36px', d: 'retro' })} alt={data.nickname}></img>
+                  <img src={gravatar.url(userData.nickname, { s: '36px', d: 'retro' })} alt={userData.nickname}></img>
                   <div>
-                    <span id="profile-name">{data.nickname}</span>
+                    <span id="profile-name">{userData.nickname}</span>
                     <span id="profile-active">Active</span>
                   </div>
                 </ProfileModal>
@@ -69,7 +130,16 @@ const Workspace: React.FC<React.PropsWithChildren> = ({ children }) => {
       </Header>
       <button onClick={onLogout}>로그아웃</button>
       <WorkspaceWrapper>
-        <Workspaces>test</Workspaces>
+        <Workspaces>
+          {userData?.Workspaces.map((ws) => {
+            return (
+              <Link key={ws.id} to={`/workspace/${123}/chaanel/일반`}>
+                <WorkspaceButton>{ws.name.slice(0, 1).toUpperCase()}</WorkspaceButton>
+              </Link>
+            );
+          })}
+          <AddButton onClick={onClickCreateWorkspace}>+</AddButton>
+        </Workspaces>
         <Channels>
           <WorkspaceName>Slack Study</WorkspaceName>
           <MenuScroll></MenuScroll>
@@ -83,6 +153,19 @@ const Workspace: React.FC<React.PropsWithChildren> = ({ children }) => {
         </Chats>
       </WorkspaceWrapper>
       {children}
+      <Modal show={ShowCreateWorkspaceModal} onCloseModal={onCloseModal}>
+        <form onSubmit={onCreateWorkspace}>
+          <label id="workspace-label">
+            <span>워크스페이스 이름</span>
+            <Input id="workspace" value={newWorkspace} onChange={onChangeNewWorkspace} />
+          </label>
+          <label id="workspace-url-label">
+            <span>워크스페이스 url</span>
+            <Input id="workspace" value={newUrl} onChange={onChangeNewUrl} />
+          </label>
+          <Button type="submit">생성하기</Button>
+        </form>
+      </Modal>
     </div>
   );
 };
