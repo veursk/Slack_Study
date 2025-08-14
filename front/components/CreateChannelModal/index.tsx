997496d67880ba3723f18fc47057192e5dc342b2
leo <veursk@gmail.com>
2025-08-14 21:07:22 +0900

    feat: 채널생성모달, 채널/워크스페이스 초대 모달

--- front/components/CreateChannelModal/index.tsx
+++ front/components/CreateChannelModal/index.tsx
@@ -2,23 +2,65 @@ import React, { useCallback } from 'react';
 import Modal from '@components/Modal';
 import { Button, Input } from '@pages/SignUp/styles';
 import useInput from '@hooks/useInput';
+import axios from 'axios';
+import { useParams } from 'react-router';
+import { toast } from 'react-toastify';
+import useSWR from 'swr';
+import { IChannel, IUser } from '@typings/db';
+import fetcher from '@utils/fetcher';
 
 interface Props {
   show: boolean;
   onCloseModal: () => void;
+  setShowCreateChannelModal: (flag: boolean) => void;
 }
 
-const CreateChannelModal: React.FC<Props> = ({ show, onCloseModal }) => {
-  const [newChannel, onChangeNewChannel] = useInput('');
-  const onCreateWorkspace = useCallback(() => {}, []);
+const CreateChannelModal: React.FC<Props> = ({ show, onCloseModal, setShowCreateChannelModal }) => {
+  const [newChannel, onChangeNewChannel, setNewChannel] = useInput('');
+  const { workspace } = useParams<{
+    workspace: string;
+  }>();
+
+  const { data: userData } = useSWR<IUser | false>('http://localhost:3095/api/users', fetcher);
+
+  const { data: channelData, mutate: mutateChannel } = useSWR<IChannel[]>(
+    userData ? `http://localhost:3095/api/workspaces/${workspace}/channels` : null,
+    fetcher,
+  );
+
+  const onCreateChannel = useCallback(
+    (e: any) => {
+      e.preventDefault();
+      axios
+        .post(
+          `http://localhost:3095/api/workspaces/${workspace}/channels`,
+          {
+            name: newChannel,
+          },
+          {
+            withCredentials: true,
+          },
+        )
+        .then(() => {
+          setShowCreateChannelModal(false);
+          mutateChannel();
+          setNewChannel('');
+        })
+        .catch((error) => {
+          console.dir(error);
+          toast.error(error.response.data, { position: 'bottom-center' });
+        });
+    },
+    [newChannel, workspace],
+  );
 
   if (!show) return null;
 
   return (
     <Modal show={show} onCloseModal={onCloseModal}>
-      <form onSubmit={onCreateWorkspace}>
+      <form onSubmit={onCreateChannel}>
         <label id="channel-label">
-          <span>워크스페이스 이름</span>
+          <span>채널 이름</span>
           <Input id="channel" value={newChannel} onChange={onChangeNewChannel} />
         </label>
 
