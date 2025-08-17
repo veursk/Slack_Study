3112b83d67b36aaa1ff025910e066ab08361416f
leo <veursk@gmail.com>
2025-08-17 16:35:49 +0900

    feat: Channel, DirectMessage 페이지 추가

--- front/pages/Channel/index.tsx
+++ front/pages/Channel/index.tsx
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { Container, Header } from './styles';
+import useInput from '@hooks/useInput';
+import ChatList from '@components/ChatList';
+import ChatBox from '@components/ChatBox';
 
 const Channel = () => {
+  const [chat, onChangeChat, setChat] = useInput('');
+
+  const onSubmitForm = useCallback((e: any) => {
+    e.preventDefault();
+    console.log('submit');
+    setChat('');
+  }, []);
+
   return (
     <Container>
       <Header>채널!</Header>
+      <ChatList />
+      <ChatBox chat={chat} onChangeChat={onChangeChat} onSubmitForm={onSubmitForm} placeholder="채팅을 입력하세요" />
     </Container>
   );
 };
--- front/pages/DirectMessage/index.tsx
+++ front/pages/DirectMessage/index.tsx
@@ -1,7 +1,66 @@
-import React from 'react';
+import { Container } from '@pages/Channel/styles';
+import React, { useCallback } from 'react';
+import { Header } from './styles';
+import gravatar from 'gravatar';
+import useSWR from 'swr';
+import { IDM, IUser } from '@typings/db';
+import fetcher from '@utils/fetcher';
+import { useParams } from 'react-router';
+// import ChatList from '@components/ChatList';
+import ChatBox from '@components/ChatBox';
+import useInput from '@hooks/useInput';
+import ChatList from '@components/ChatList';
+import axios from 'axios';
 
 const DirectMessage = () => {
-  return <div>DM</div>;
+  const { workspace, id } = useParams<{ workspace: string; id: string }>();
+  const { data: userData } = useSWR(`http://localhost:3095/api/workspaces/${workspace}/members/${id}`, fetcher);
+  const { data: myData } = useSWR(`http://localhost:3095/api/users`, fetcher);
+  const [chat, onChangeChat, setChat] = useInput('');
+  const { data: chatData, mutate: mutateChat } = useSWR<IDM[]>(
+    `http://localhost:3095/api/workspaces/${workspace}/dms/${id}/chats?perPage=20&page=1`,
+    fetcher,
+  );
+
+  const onSubmitForm = useCallback(
+    (e: any) => {
+      e.preventDefault();
+      console.log(chat);
+      if (chat?.trim()) {
+        axios
+          .post(
+            `http://localhost:3095/api/workspaces/${workspace}/dms/${id}/chats`,
+            {
+              content: chat,
+            },
+            {
+              withCredentials: true,
+            },
+          )
+          .then(() => {
+            mutateChat();
+            setChat('');
+          })
+          .catch((error) => console.log(error));
+      }
+    },
+    [chat],
+  );
+
+  if (!userData || !myData) {
+    return null;
+  }
+
+  return (
+    <Container>
+      <Header>
+        <img src={gravatar.url(userData.email, { s: '24px', d: 'retro' })} alt={userData.nickname}></img>
+        <span>{userData.nickname}</span>
+      </Header>
+      <ChatList />
+      <ChatBox chat={chat} onChangeChat={onChangeChat} onSubmitForm={onSubmitForm} placeholder="채팅을 입력하세요" />
+    </Container>
+  );
 };
 
 export default DirectMessage;
