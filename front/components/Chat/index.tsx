d8fc8654e61e909c11b0d220b045844177527c5e
Leo <hbcho@sbs.co.kr>
2025-08-18 13:28:37 +0900

    feat: 정규표현식으로 @멘션부분 핸들링

--- front/components/Chat/index.tsx
+++ front/components/Chat/index.tsx
@@ -1,15 +1,39 @@
-import React from 'react';
+import React, { memo, useMemo } from 'react';
 import { ChatWrapper } from './styles';
 import gravatar from 'gravatar';
 import { IDM } from '@typings/db';
 import dayjs from 'dayjs';
+import regexifyString from 'regexify-string';
+import { Link } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 
 interface Props {
   data: IDM;
 }
 
-const Chat: React.FC<Props> = ({ data }) => {
+const Chat: React.FC<Props> = memo(({ data }) => {
+  const { workspace } = useParams<{ workspace: string }>();
   const user = data.Sender;
+
+  const result = useMemo(
+    () =>
+      regexifyString({
+        pattern: /@\[(.+?)]\((\d+?)\)|\n/g,
+        decorator(match, index) {
+          const arr: string[] | null = match.match(/@\[(.+?)]\((\d+?)\)/)!;
+          if (arr) {
+            return (
+              <Link key={match + index} to={`/workspace/${workspace}/dm/${arr[2]}`}>
+                @{arr[1]}
+              </Link>
+            );
+          }
+          return <br key={index} />;
+        },
+        input: data.content,
+      }),
+    [data.content],
+  );
 
   return (
     <ChatWrapper>
@@ -21,10 +45,10 @@ const Chat: React.FC<Props> = ({ data }) => {
           <b>{user.nickname}</b>
           <span>{dayjs(data.createdAt).format('h:mm A')}</span>
         </div>
-        <p>{data.content}</p>
+        <p>{result}</p>
       </div>
     </ChatWrapper>
   );
-};
+});
 
 export default Chat;
